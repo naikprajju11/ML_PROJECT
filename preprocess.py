725308535fd919f7773d7703689c68997840a8ce
prajjunaik41 <prajwalgnaik333@gmail.com>
2026-01-29 21:57:28 +0530

    added files

--- preprocess.py
+++ preprocess.py
@@ -55,3 +55,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 print(f"Training set shape: {X_train.shape}")
 print(f"Test set shape: {X_test.shape}")
 
+ 
+X_train.to_csv('data/X_train.csv', index=False)
+X_test.to_csv('data/X_test.csv', index=False)  
+y_train.to_csv('data/y_train.csv', index=False)
+y_test.to_csv('data/y_test.csv', index=False)
